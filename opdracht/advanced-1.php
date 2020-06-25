f63a03df304655b863adebe235a8feaa6337613d
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-06-25 11:50:26 +0200

    update 2

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -8,22 +8,25 @@
 <body>
  
 <form action="advanced-2.php" method="post">
-    Tekstkleur: <label for="color"></label><br>
+    Tekstkleur: <label for="color"> </label><br>
+    <select name="color">
     <?php 
         $kleuren = array("Red", "Blue", "Green", "Black", "Brown");
         foreach($kleuren as $kleur){
-            echo "<input type='checkbox' name='color' value='".$kleur."'>".$kleur."<br>";
+            echo "<option value='".$kleur."'>".$kleur."</option>";
         }
     ?>
-    Achtergrondkleur: <label for="backgroundColor"><br>
+    </select></br>
+    Achtergrondkleur: <label for="backgroundColor"> </label><br>
+    <select name="backgroundColor">
     <?php 
-        $kleuren = array("Red", "Blue", "Green", "Black", "Brown");
         foreach($kleuren as $kleur){
-            echo "<input type='checkbox' name='backgroundColor' value='".$kleur."'>".$kleur."<br>";
+            echo "<option value='".$kleur."'>".$kleur."</option>";
         }
     ?>
-    Padding <input type="text" name="padding"><br>
-    Tabel-border: <input type="text" name="tabelBorder"><br>
+    </select></br>
+    Padding <input type="number" name="padding"><br>
+    Tabel-border: <input type="number" name="tabelBorder"><br>
     <input type="submit">
 </form>
 
